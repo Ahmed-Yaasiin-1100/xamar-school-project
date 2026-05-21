a4d71cf6d0b6b865e5fc68f9523797b8ef0dd37c
Ahmed Yaasiin <engmaadka@gmail.com>
2026-05-21 13:59:57 -0700

    apdate students page

--- xamarschoolproject/src/Dashboard/Students.jsx
+++ xamarschoolproject/src/Dashboard/Students.jsx
@@ -1,10 +1,71 @@
 import React from 'react'
 import SideDashboard from './SideDashboard'
+// import Dashboard from './Dashboard'
+import TotalStudents from './TotalStudents'
 
 function Students() {
+  let students = [
+    {
+      id : "C126",
+      name : "Qaalid Maxamed Yaasiin",
+      gender : "Male",
+      location : "Waaberi",
+      Action : ["Update", "Delete"]
+    },
+    {
+      id : "C226",
+      name : "Farxiyo Cali Yuusuf",
+      gender : "Female",
+      location : "Hodan",
+      Action : ["Update", "Delete"]
+    },
+    {
+      id : "C326",
+      name : "Cumar Xirsi Daahir",
+      gender : "Male",
+      location : "Shangani",
+      Action : ["Update", "Delete"]
+    }
+
+  ]
   return (
     <>
      <SideDashboard/>
+
+      <TotalStudents total={students.length}/>
+      
+      <div className="table">
+      <table>
+
+        <thead>
+          <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Gender</th>
+          <th>Location</th>
+          <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+           students.map((st)=>(
+             <tr>
+            <td>{st.id}</td>
+            <td>{st.name}</td>
+            <td>{st.gender}</td>
+            <td>{st.location}</td>
+            <td style={{display:"flex", gap: "10px"}}><span style={{backgroundColor:"green", padding:"5px", borderRadius:"10px", color:"white"}}>{st.Action[0]}</span> <span style={{backgroundColor:"red", padding:"5px", borderRadius:"10px", color:"white"}}>{st.Action[1]}</span></td>
+          </tr>
+           ))
+          }
+          
+         
+        </tbody>
+      </table>
+
+         
+
+    </div>
     </>
   )
 }
---
+++ xamarschoolproject/src/Dashboard/TotalStudents.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function TotalStudents({total}) {
+  return (
+    <>
+    <div>{total}</div>
+    console.log(total);
+    </>
+    
+  )
+}
+
+export default TotalStudents
